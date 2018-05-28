f7ddb50fb9f561d24890829a6230cc25ae8ba235
aman25code <amanmaheshwari25@gmail.com>
2018-05-28 11:26:16 -0700

    added for plot data for classification and sigmoid function

--- Assignment2/machine-learning-ex2/ex2/sigmoid.m
+++ Assignment2/machine-learning-ex2/ex2/sigmoid.m
@@ -10,8 +10,12 @@ g = zeros(size(z));
 %               vector or scalar).
 
 
-
-
+[m,n] = size(z);
+for i = 1:m
+    for j = 1:n
+        g(i,j) = 1/(1+exp(-z(i,j)));
+    end
+end    
 
 % =============================================================
 
